150c8f33cc3f6de050cb821690d91b10b317d0c8
Jean-François Primeau <jeanfrancois@genielab.co>
2022-05-04 10:00:52 -0400

    Updated GPS code for activity 1, code for act 2 still buggy. Updated md 1

--- code/CompassWithGPSData.py
+++ code/CompassWithGPSData.py
@@ -1,7 +1,13 @@
 # import libraries we will be using
 from microbit import *
 import math
-import speech
+import radio
+#import speech
+
+BUFFLEN = 128
+
+radio.config(length=BUFFLEN)
+radio.on()
 
 # Variables and constants
 
@@ -9,15 +15,15 @@ import speech
 currentLocation = [0.0, 0.0]
 
 # this is where we want to go, needs to be formatted for the GPS module in Degrees Minutes, no space.
-DESTINATION = (4531.82276, -7333.06061)
+DESTINATION = (45.52837212770099, -73.5524555128115)
 
-# How close we need to be to the destination to trigger our "We're here" message
-DIST_THRESH = 1000.0
+# How close we need to be to the destination to trigger our "We're here" message, in meters
+DIST_THRESH = 10.0
 
 # to assemble the incoming data from the GPS module
 msg = ""
 
-# to store the message
+# to store the message and current location
 listeNMEA = []
 
 # True to enable GPS position detection
@@ -34,7 +40,7 @@ def initCompass():
     compass.calibrate()
 
 
-def angleFromCoordinate(lat1, long1, lat2, long2):
+def angleFromCoordinate2(lat1, long1, lat2, long2):
     # calculate the angle between our current position and the target location
     # this will return the bearing relative to the North
     dLon = long2 - long1
@@ -44,16 +50,33 @@ def angleFromCoordinate(lat1, long1, lat2, long2):
         dLon
     )
 
-    brng = math.atan2(y, x)
+    brng1 = math.atan2(y, x)
 
-    brng = math.degrees(brng)
-    brng = (brng + 360) % 360
-    brng = 360 - brng  # count degrees clockwise - remove to make counter-clockwise
+    brng = (brng1*180/math.pi + 360) % 360
+    #brng = 360 - brng  # count degrees clockwise - remove to make counter-clockwise
 
     # bring between -180 and 180, 0 being North
     if brng > 180:
         brng = -(360 - brng)
 
+    return brng
+
+def angleFromCoordinate (lat1, lon1, lat2, lon2):
+
+    y = math.cos(lat2) * math.sin(lon2-lon1)
+    x = (math.cos(lat1) * math.sin(lat2)) - (math.sin(lat1) * math.cos(lat2) * math.cos(lon2-lon1))
+
+    brng = (math.atan2(y,x) * 180/math.pi + 360) % 360
+
+    #brng = math.degrees(brng)
+    #brng = (brng + 360) % 360
+    #brng = 360 - brng  # count degrees clockwise - remove to make counter-clockwise
+
+    # bring between -180 and 180, 0 being North
+    if brng > 180:
+        brng = -(360 - brng)
+
+    #sendNMEA("$" + str(brng))
     return brng
 
 
@@ -65,37 +88,45 @@ def currentHeading():
     return heading
 
 
+def sendNMEA(m):
+    if m != "" and m[0] == '$':
+        if m[-1] == "\n":
+            radio.send(m)
+        else:
+            radio.send(m+"\n")
+
+
 def displayDirection():
     # once we have our current position, display an arrow pointing to the target GPS location
     direction = angleFromCoordinate(
         currentLocation[0], currentLocation[1], DESTINATION[0], DESTINATION[1]
     )
 
-    azimut = currentHeading() - direction
+    azimut = direction - currentHeading()
     # make sure it works with range -180 to 180
     if azimut < -180:
         azimut += 360
 
-    # E and W must be inverted
-    # if -22.5 <= azimut < 22.5:
-    #     display.show(Image.ARROW_N)
-    # elif 22.5 <= azimut < 67.5:
-    #     display.show(Image.ARROW_NW)
-    # elif 67.5 <= azimut < 112.5:
-    #     display.show(Image.ARROW_W)
-    # elif 112.5 <= azimut < 157.5:
-    #     display.show(Image.ARROW_SW)
-    # elif 157.5 <= azimut <= 180 or -180 <= azimut < -157.5:
-    #     display.show(Image.ARROW_S)
-    # elif -157.5 <= azimut < -112.5:
-    #     display.show(Image.ARROW_SE)
-    # elif -112.5 <= azimut < -67.5:
-    #     display.show(Image.ARROW_E)
-    # elif -67.5 <= azimut < -22.5:
-    #     display.show(Image.ARROW_NE)
-    # else:
-    #     # is all is working properly, this should never display
-    #     display.show(Image.CONFUSED)
+    #E and W must be inverted
+    if -22.5 <= azimut < 22.5:
+        display.show(Image.ARROW_N)
+    elif 22.5 <= azimut < 67.5:
+        display.show(Image.ARROW_NW)
+    elif 67.5 <= azimut < 112.5:
+        display.show(Image.ARROW_W)
+    elif 112.5 <= azimut < 157.5:
+        display.show(Image.ARROW_SW)
+    elif 157.5 <= azimut <= 180 or -180 <= azimut < -157.5:
+        display.show(Image.ARROW_S)
+    elif -157.5 <= azimut < -112.5:
+        display.show(Image.ARROW_SE)
+    elif -112.5 <= azimut < -67.5:
+        display.show(Image.ARROW_E)
+    elif -67.5 <= azimut < -22.5:
+        display.show(Image.ARROW_NE)
+    else:
+        # is all is working properly, this should never display
+        display.show(Image.CONFUSED)
 
 
 def initGPS():
@@ -103,7 +134,7 @@ def initGPS():
     uart.init(baudrate=9600, bits=8, parity=None, stop=1, tx=pin1, rx=pin2)
     sleep(500)
     INIT_SEQUENCE_RMC = [
-        b"\xB5\x62\x06\x08\x06\x00\xE8\x03\x01\x00\x01\x00\x01\x39",                 # Frequence 1s
+        b"\xB5\x62\x06\x08\x06\x00\xE8\x03\x01\x00\x01\x00\x01\x39",                 # Frequence 1hz
         #b"\xB5\x62\x06\x08\x06\x00\xC8\x00\x01\x00\x01\x00\xDE\x6A\xB5\x62\x06\x08\x00\x00\x0E\x30",                 # Frequence 5Hz
         b"\x24\x45\x49\x47\x50\x51\x2c\x44\x54\x4d\x2a\x33\x42\x0d\x0a\xb5\x62\x06\x01\x03\x00\xf0\x0a\x00\x04\x23",  # Disable GPDTM
         b"\x24\x45\x49\x47\x50\x51\x2c\x47\x42\x53\x2a\x33\x30\x0d\x0a\xb5\x62\x06\x01\x03\x00\xf0\x09\x00\x03\x21",  # Disable GPGBS
@@ -122,29 +153,8 @@ def initGPS():
         sleep(100)
 
 
-### BUG HERE !!!!
-def getCurrentLocation():
-    # parse the incoming message from the GPS module
-    #if len(listeNMEA) > 0:
-        # make sure there is a valid message
-    if len(listeNMEA[0]) >= 45:
-        if listeNMEA[-1][0:5] == "$GPRMC":
-            if listeNMEA[-1][17] == "A":
-                currentLocation[0] = float(listeNMEA[-1][19:28])
-                if listeNMEA[-1][30] == "S":
-                    currentLocation[0] = -currentLocation[0]
-                currentLocation[1] = float(listeNMEA[-1][32:42])
-                if listeNMEA[-1][44] == "W":
-                    currentLocation[1] = -currentLocation[1]
-                display.show(Image.YES)
-        else:
-            display.show(Image.TRIANGLE_LEFT)
-    else:
-        display.show(Image.TRIANGLE)
-    #else:
-        #display.show(Image.NO)
-
-
+def converterDM_DD(coordToConvert) :
+    return (((coordToConvert-coordToConvert%100)/100) + (coordToConvert%100)/60)
 
 
 def soundWhenClose(message):
@@ -156,7 +166,7 @@ def soundWhenClose(message):
         #(((currentLocation[1]== DESTINATION[1]) & (currentLocation[1]==0)) | (currentLocation[1] * DESTINATION[1] > 0)) :
 
         # if we are close enough to the spot, a message will be heard, emitted from the micro:bit's speaker
-        speech.say(message, pitch=120, speed=120, mouth=150, throat=170)
+        #speech.say(message, pitch=120, speed=120, mouth=150, throat=170)
         display.show(Image.TARGET)
 
         sleep(5000)
@@ -171,22 +181,32 @@ def soundWhenClose(message):
 initCompass()
 initGPS()
 
-speech.say(SM_START, pitch=120, speed=120, mouth=150, throat=170)
+#speech.say(SM_START, pitch=120, speed=120, mouth=150, throat=170)
 
 while True:
 
     # first get location from GPS
     if captureMode :
         if uart.any():
-            incoming = uart.readline().decode().format("%s")
+            incoming = uart.readline().format("%s")
             sp=incoming.split("$")
             msg += sp[0]
             if len(sp)>1:
                 for m in sp[1:]:
                     listeNMEA.append(msg)
                     msg="$"+m
-            getCurrentLocation()
-            #listeNMEA.clear()
+                    if len(listeNMEA) > 1:
+                        splitlist = listeNMEA[-1].split(",")
+                        if len(splitlist) > 3:
+                            if splitlist[0] == "$GPRMC":
+                                coords = [converterDM_DD(float(splitlist[3])), converterDM_DD(float(splitlist[5]))]
+                                if splitlist[4] == "S" :
+                                    coords[0] = -coords[0]
+                                if splitlist[6] == "W" :
+                                    coords[1] = -coords[1]
+                                currentLocation.append(coords)
+        if len(listeNMEA) > 2:
+            listeNMEA.clear()
 
     # then see if we are where we want to go to
     if soundWhenClose(SM_END):
@@ -194,4 +214,6 @@ while True:
 
     # and finally display the direction of the objective if we are not there yet
     displayDirection()
+    sleep(100)
+
 
